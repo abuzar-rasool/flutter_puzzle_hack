72b55c5473d9b6b6e337dd5802d6f84f5e688c71
FaazAbidi <sa06195@st.habib.edu.pk>
2022-02-26 02:59:41 +0500

    sliding image for single block

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,24 @@
+
 import 'package:flutter/material.dart';
 
 const Size imageSize = Size(150, 95);
+const Size boardSize = Size(500, 500);
+const center =  Offset(250, 250);
+Offset init = Offset(center.dx - imageSize.width / 2, center.dy - imageSize.height / 2);
+Offset offset = Offset(imageSize.width - 2, imageSize.height - 9);
+
+
+List<Offset> blockOffsets = [
+      // first row
+      Offset(init.dx, init.dy - offset.dy),
+      Offset(init.dx + offset.dx / 2, init.dy - offset.dy / 2),
+      Offset(init.dx + offset.dx, init.dy),
+      //second row
+      Offset(init.dx - offset.dx / 2, init.dy - offset.dy / 2),
+      Offset(init.dx, init.dy),
+      Offset(init.dx + offset.dx / 2, init.dy + offset.dy / 2),
+      //thrid row
+      Offset(init.dx - offset.dx, init.dy),
+      Offset(init.dx - offset.dx / 2, init.dy + offset.dy / 2),
+      Offset(init.dx, init.dy + offset.dy),
+    ];
--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:puzzle_hack/board_view.dart';
+import 'package:puzzle_hack/tiles.dart';
 
-void main() {
-  runApp(const Home());
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: BoardView(),
-      ),
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      body: Tiles(),
     );
   }
 }
