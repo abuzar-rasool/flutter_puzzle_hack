7a501ce0861d9409afe144a93437362f3583f7bf
abuzarrasool <abuzar@oraan.com>
2022-03-15 03:43:22 +0500

    dict

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 const Size kImageSize = Size(150, 95);
 const Size kBoardSize = Size(500, 400);
 const double kBlockOpacity = 0;
+const Map<String, dynamic> kImages = {
+  'block_1' : '',
+  'block_2' : '',
+  'block_3' : '',
+  'block_4' : '',
+  'block_5' : '',
+  'block_6' : '',
+  'block_7' : '',
+  'block_8' : '',
+  'empty' : '',
+  'solved' : '',
+};
